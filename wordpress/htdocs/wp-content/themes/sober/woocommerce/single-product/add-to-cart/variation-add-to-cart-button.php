76a76f966b427752d2356b3abc65c7728159add3
qingshan <lichao@bigbaser.com>
2017-11-23 11:14:23 +0800

    test

--- wordpress/htdocs/wp-content/themes/sober/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ wordpress/htdocs/wp-content/themes/sober/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -32,12 +32,12 @@ global $product;
 	do_action( 'woocommerce_after_add_to_cart_quantity' );
 	?>
 
-	<!--<button type="submit" class="single_add_to_cart_button button alt">
+	<button type="submit" class="single_add_to_cart_button button alt">
 		<svg viewBox="0 0 20 20">
 			<use xlink:href="#basket-addtocart"></use>
 		</svg>
-		<?php /*echo esc_html( $product->single_add_to_cart_text() ); */?>
-	</button>-->
+		<?php echo esc_html( $product->single_add_to_cart_text() ); ?>
+	</button>
     <a href="<?php echo esc_url( wc_get_checkout_url() ); ?>" class="button checkout wc-forward">
         <span class="subtotal"><?php echo WC()->cart->get_cart_subtotal() ?></span>
         <span><?php esc_html_e( 'Checkout', 'sober' ); ?></span>
