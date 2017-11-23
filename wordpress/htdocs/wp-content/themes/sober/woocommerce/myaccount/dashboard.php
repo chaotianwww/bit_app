673080029bbcf72556781dcc0d0df9aca136b8ec
qingshan <lichao@bigbaser.com>
2017-11-23 09:55:44 +0800

    test

--- wordpress/htdocs/wp-content/themes/sober/woocommerce/myaccount/dashboard.php
+++ wordpress/htdocs/wp-content/themes/sober/woocommerce/myaccount/dashboard.php
@@ -101,7 +101,7 @@ if ( ! defined( 'ABSPATH' ) ) {
            window.open('mailto:uncle.cyan@gmail.com?subject=spade.com&body='+$("#raf-message a").html());
        });
 
-        twttr.widgets.createShareButton(
+      /*  twttr.widgets.createShareButton(
             $("#raf-message a").html(),
             document.getElementById("share_twitter"),
             {
@@ -112,7 +112,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                 related: "twitterapi,twitter"
             }
         );
-    });
+    });*/
 
 
 </script>
