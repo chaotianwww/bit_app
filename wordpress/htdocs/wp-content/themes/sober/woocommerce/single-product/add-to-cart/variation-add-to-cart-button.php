fe183f5868a34a510f5f7ebfdab68972a5c7186f
qingshan <lichao@bigbaser.com>
2017-11-23 11:21:42 +0800

    test

--- wordpress/htdocs/wp-content/themes/sober/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ wordpress/htdocs/wp-content/themes/sober/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -33,14 +33,15 @@ global $product;
 
 	<button type="submit" class="single_add_to_cart_button button alt">
 		<svg viewBox="0 0 20 20">
-			<use xlink:href="#basket-addtocart"></use>
+			<!--<use xlink:href="#basket-addtocart"></use>-->
+            <?php esc_html_e( 'Checkout', 'sober' ); ?>
 		</svg>
 		<?php echo esc_html( $product->single_add_to_cart_text() ); ?>
 	</button>
-    <a href="<?php echo esc_url( wc_get_checkout_url() ); ?>" class="button checkout wc-forward">
-        <span class="subtotal"><?php echo $product->get_min_purchase_quantity ?></span>
-        <span><?php esc_html_e( 'Checkout', 'sober' ); ?></span>
-    </a>
+    <!--<a href="<?php /*echo esc_url( wc_get_checkout_url() ); */?>" class="button checkout wc-forward">
+        <span class="subtotal"><?php /*echo $product->get_min_purchase_quantity */?></span>
+        <span></span>
+    </a>-->
 	<input type="hidden" name="add-to-cart" value="<?php echo absint( $product->get_id() ); ?>" />
 	<input type="hidden" name="product_id" value="<?php echo absint( $product->get_id() ); ?>" />
 	<input type="hidden" name="variation_id" class="variation_id" value="0" />
