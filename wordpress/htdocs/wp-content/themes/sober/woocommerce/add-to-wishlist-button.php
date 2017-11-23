02ef31bfc9f01053d4299c8e2a4ed54042935328
qingshan <lichao@bigbaser.com>
2017-11-23 18:32:54 +0800

    test

--- wordpress/htdocs/wp-content/themes/sober/woocommerce/add-to-wishlist-button.php
+++ wordpress/htdocs/wp-content/themes/sober/woocommerce/add-to-wishlist-button.php
@@ -14,9 +14,10 @@ if ( ! defined( 'YITH_WCWL' ) ) {
 global $product;
 ?>
 
-<a href="<?php echo esc_url( add_query_arg( 'add_to_wishlist', $product_id ) ) ?>" rel="nofollow" data-product-id="<?php echo esc_attr( $product_id ) ?>" data-product-type="<?php echo esc_attr( $product_type ) ?>" class="<?php echo esc_attr( $link_classes ) ?>">
+<!--<a href="<?php /*echo esc_url( add_query_arg( 'add_to_wishlist', $product_id ) ) */?>" rel="nofollow" data-product-id="<?php /*echo esc_attr( $product_id ) */?>" data-product-type="<?php /*echo esc_attr( $product_type ) */?>" class="<?php /*echo esc_attr( $link_classes ) */?>">
 	<svg viewBox="0 0 20 20">
 		<use xlink:href="#heart-wishlist-like"></use>
 	</svg>
-	<span class="screen-reader-text"><?php echo esc_html( $label ) ?></span>
+	<span class="screen-reader-text"><?php /*echo esc_html( $label ) */?></span>
 </a>
+-->
