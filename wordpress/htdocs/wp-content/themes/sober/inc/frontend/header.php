ffa82a904baf81d5dbc1b415b0cf6630fc2b65c3
qingshan <lichao@bigbaser.com>
2017-11-23 12:04:22 +0800

    test

--- wordpress/htdocs/wp-content/themes/sober/inc/frontend/header.php
+++ wordpress/htdocs/wp-content/themes/sober/inc/frontend/header.php
@@ -67,7 +67,7 @@ function sober_scripts() {
 	if ( sober_get_option( 'added_to_cart_notice' ) ) {
 		wp_enqueue_script( 'notify' );
 	}
-    $version = "2.0.2";
+    $version = "2.1.0";
 
 	wp_register_script( 'sober', get_template_directory_uri() . '/js/script.js', array(
 		'jquery',
