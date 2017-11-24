b0f1ccbf53caf57ce867f5466c30d31969069aaf
qingshan <lichao@bigbaser.com>
2017-11-24 17:34:28 +0800

    test

--- wordpress/htdocs/wp-content/themes/sober/woocommerce/myaccount/form-login.php
+++ wordpress/htdocs/wp-content/themes/sober/woocommerce/myaccount/form-login.php
@@ -221,6 +221,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 </div>
 
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
     <script>
         $(document).ready(function(){
             var tmpHtml =$(".the_champ_outer_login_container").html();
