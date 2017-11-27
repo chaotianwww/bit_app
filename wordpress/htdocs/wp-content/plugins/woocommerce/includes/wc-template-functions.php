d08dbe12e508f7c5e343affd2a750bb8bab78810
qingshan <lichao@bigbaser.com>
2017-11-27 13:13:54 +0800

    test

--- wordpress/htdocs/wp-content/plugins/woocommerce/includes/wc-template-functions.php
+++ wordpress/htdocs/wp-content/plugins/woocommerce/includes/wc-template-functions.php
@@ -29,8 +29,8 @@ function wc_template_redirect() {
 	} elseif ( is_page( wc_get_page_id( 'checkout' ) ) && wc_get_page_id( 'checkout' ) !== wc_get_page_id( 'cart' ) && WC()->cart->is_empty() && empty( $wp->query_vars['order-pay'] ) && ! isset( $wp->query_vars['order-received'] ) ) {
 
 		// When on the checkout with an empty cart, redirect to cart page.
-		wc_add_notice( __( 'Checkout is not available whilst your cart is empty.', 'woocommerce' ), 'notice' );
-		wp_redirect( wc_get_page_permalink( 'cart' ) );
+		//wc_add_notice( __( 'Checkout is not available whilst your cart is empty.', 'woocommerce' ), 'notice' );
+		wp_redirect( wc_get_page_permalink( 'shop' ) );
 		exit;
 
 	} elseif (
