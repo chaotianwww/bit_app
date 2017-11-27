8069582251b6faf74e9bf04239e35e44409b7147
qingshan <lichao@bigbaser.com>
2017-11-27 11:51:55 +0800

    test

--- wordpress/htdocs/wp-content/themes/sober/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ wordpress/htdocs/wp-content/themes/sober/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -26,7 +26,7 @@ global $product;
 	) );*/
     ?>
 
-    <a  type="button"  class="check_this price direct-purchase"  href="<?php echo esc_url( wc_get_checkout_url()).'?add-to-cart='.absint( $product->get_id() ); ?>?"><?php esc_html_e( '直购价　$'.$product->price*10, 'sober' ); ?></a>
+    <button  type="submit"  class="checkout_this  price direct-purchase" name="checkout"><?php esc_html_e( '直购价　$'.$product->price*10, 'sober' ); ?></button>
     <?php
 
 	/**
@@ -34,7 +34,7 @@ global $product;
 	 */
 	do_action( 'woocommerce_after_add_to_cart_quantity' );
 	?>
-	<button type="button" class="check_this price friendeal" >
+	<button type="submit" class="checkout_this  price friendeal" name="checkout">
         <?php esc_html_e( '拼单价　$'.$product->price, 'sober' ); ?>
 	</button>
     <!--<a href="<?php /*echo esc_url( wc_get_checkout_url() ); */?>" class="button checkout wc-forward">
@@ -44,6 +44,7 @@ global $product;
 	<input type="hidden" name="add-to-cart" value="<?php echo absint( $product->get_id() ); ?>" />
 	<input type="hidden" name="product_id" value="<?php echo absint( $product->get_id() ); ?>" />
 	<input type="hidden" name="variation_id" class="variation_id" value="0" />
+    <input type="hidden" name="checkout_url" value="<?php echo esc_url( wc_get_checkout_url()); ?>"/>
 
 
 </div>
