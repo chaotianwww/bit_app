72c98774bcdcf04e8dd91d53abedb6a8aee4e8a3
qingshan <lichao@bigbaser.com>
2017-11-27 14:30:21 +0800

    test

--- wordpress/htdocs/wp-content/plugins/woocommerce/includes/wc-template-functions.php
+++ wordpress/htdocs/wp-content/plugins/woocommerce/includes/wc-template-functions.php
@@ -28,8 +28,8 @@ function wc_template_redirect() {
 
 	} elseif ( is_page( wc_get_page_id( 'checkout' ) ) && wc_get_page_id( 'checkout' ) !== wc_get_page_id( 'cart' ) && WC()->cart->is_empty() && empty( $wp->query_vars['order-pay'] ) && ! isset( $wp->query_vars['order-received'] ) ) {
 
-        echo $_GET['add_to_cart'];
-        print_r(WC()->cart->get_cart());exit;
+        $wc = new WC_Cart();
+        $wc->add_to_cart($_GET['add_to_cart']);
 		// When on the checkout with an empty cart, redirect to cart page.
 		//wc_add_notice( __( 'Checkout is not available whilst your cart is empty.', 'woocommerce' ), 'notice' );
 		//wp_redirect( wc_get_page_permalink( 'product' ) );
