9b1667a279df8cf2e634be0e031319ecce80b4ff
qingshan <lichao@bigbaser.com>
2017-11-27 18:55:43 +0800

    test

--- wordpress/htdocs/wp-content/plugins/refer-a-friend-for-woocommerce-by-wpgens/public/class-gens-raf-public.php
+++ wordpress/htdocs/wp-content/plugins/refer-a-friend-for-woocommerce-by-wpgens/public/class-gens-raf-public.php
@@ -271,11 +271,13 @@ class Gens_RAF_Public {
 	<?php
 	}
 
-    public function account_page_show_order_link($order) {
+    public function account_page_show_order_link($orders) {
 
+        foreach($orders as $key => $order){
+            $order =  $order['data'];
+        }
         $referral_id = $this->get_referral_id( get_current_user_id() );
-        $refLink = esc_url(add_query_arg( 'raf', $referral_id, get_home_url().'/product/testtestest?order_id='.$order['order_id']."&user_id=".get_current_user_id().'&items=xxx' ));
-        print_r($order);
+        $refLink = esc_url(add_query_arg( 'raf', $referral_id, get_home_url().'/product/'.$order['name'].'?order_id='.$order['order_id']."&user_id=".get_current_user_id().'&items=xxx' ));
         ?>
         <div id="raf-message" class="woocommerce-message"><?php _e( 'invite your friend to get this deal together !','gens-raf'); ?> <a href="<?php echo $refLink; ?>" ><?php echo $refLink; ?></a></div>
 
