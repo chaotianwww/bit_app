580ebe087d0e5c6454f0f9de588b6c3c5e22c996
qingshan <lichao@bigbaser.com>
2017-11-27 18:17:16 +0800

    test

--- wordpress/htdocs/wp-content/plugins/woocommerce/templates/myaccount/view-order.php
+++ wordpress/htdocs/wp-content/plugins/woocommerce/templates/myaccount/view-order.php
@@ -23,7 +23,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-<p><?php
+<p>
+    <?php  do_action( 'woocommerce_before_my_account' ); ?>
+<div class="woocommerce-share">
+    <button id="share_face_book" class="share-link"><i class="icon-facebook"></i>share to facebook</button>
+    <button id="share_twitter" class="share-link"><i class="icon-twitter"></i>share to twitter</button>
+    <button id="share_google" class="share-link"><i class="icon-google"></i>share to google+</button>
+</div>
+    <?php
 	/* translators: 1: order number 2: order date 3: order status */
 	printf(
 		__( 'Order #%1$s was placed on %2$s and is currently %3$s.', 'woocommerce' ),
@@ -52,5 +59,64 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<?php endforeach; ?>
 	</ol>
 <?php endif; ?>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<script>
+    window.twttr = (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],
+            t = window.twttr || {};
+        if (d.getElementById(id)) return t;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
 
+        t._e = [];
+        t.ready = function(f) {
+            t._e.push(f);
+        };
+
+        return t;
+    }(document, "script", "twitter-wjs"));
+
+</script>
+<script>
+
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId            : '1955847301339546',
+            autoLogAppEvents : true,
+            xfbml            : true,
+            version          : 'v2.11'
+        });
+
+        $("#share_face_book").click(function(){
+            console.log('$("#raf-message a").html():'+$("#raf-message a").html());
+            FB.ui(
+                {
+                    method: 'share',
+                    href: $("#raf-message a").html()
+                }, function(response){});
+        });
+    };
+    $(document).ready(function(){
+        $("#send_email").click(function(){
+
+            console.log('$("#raf-message a").html():'+$("#raf-message a").html());
+            window.open('mailto:uncle.cyan@gmail.com?subject=spade.com&body='+$("#raf-message a").html());
+        });
+        $(".woocommerce-message").addClass("woocommerce-message-bgcolor");
+
+    });
+
+
+</script>
 <?php do_action( 'woocommerce_view_order', $order_id ); ?>
