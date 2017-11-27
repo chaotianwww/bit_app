a8546bab96ff11696855385e3f165a5d3e3b912a
qingshan <lichao@bigbaser.com>
2017-11-27 13:08:56 +0800

    test

--- wordpress/htdocs/wp-content/themes/sober/functions.php
+++ wordpress/htdocs/wp-content/themes/sober/functions.php
@@ -21,6 +21,11 @@ function sober_content_width() {
 
 add_action( 'after_setup_theme', 'sober_content_width', 0 );
 
+add_action( 'woocommerce_add_to_cart_handler', 'empty_cart_before_add_to_cart' );
+function empty_cart_before_add_to_cart() {
+    global $woocommerce;
+    $woocommerce->cart->empty_cart();
+}
 
 if ( ! function_exists( 'sober_setup' ) ) :
 	/**
