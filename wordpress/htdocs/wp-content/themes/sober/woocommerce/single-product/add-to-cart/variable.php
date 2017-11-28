9ffc55bd3c438bd85344667d694890af5ddc66de
qingshan <lichao@bigbaser.com>
2017-11-28 15:25:46 +0800

    test

--- wordpress/htdocs/wp-content/themes/sober/woocommerce/single-product/add-to-cart/variable.php
+++ wordpress/htdocs/wp-content/themes/sober/woocommerce/single-product/add-to-cart/variable.php
@@ -19,12 +19,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 if(isset($_GET['ref'] ) && !empty($_GET['ref'] )){
-    WC()->session->set( 'ref_for_a_friends_order' , $_GET['ref'] );
+   $_SESSION['ref_for_a_friends_order'] = $_GET['ref'] ;
 }
 global $product;
 
 $attribute_keys = array_keys( $attributes );
-echo 'reffffffffffffff-----'. WC()->session->get( 'ref_for_a_friends_order');
+echo 'reffffffffffffff-----'. $_SESSION['ref_for_a_friends_order'];
 
 do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
