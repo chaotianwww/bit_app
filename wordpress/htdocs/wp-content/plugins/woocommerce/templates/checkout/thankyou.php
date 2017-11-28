f59f555248c0136bce0874ebe7562d491f83bc97
qingshan <lichao@bigbaser.com>
2017-11-28 10:38:34 +0800

    test

--- wordpress/htdocs/wp-content/plugins/woocommerce/templates/checkout/thankyou.php
+++ wordpress/htdocs/wp-content/plugins/woocommerce/templates/checkout/thankyou.php
@@ -38,9 +38,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 		<?php else : ?>
 
-        <?php
-        $order_items           = $order->get_items( apply_filters( 'woocommerce_purchase_order_item_types', 'line_item' ) );
-        do_action( 'woocommerce_after_payment_share',['order_id'=> $order->get_order_number(),'item'=>$order_items]); ?> ?>
+            <?php
+                $order_items           = $order->get_items( apply_filters( 'woocommerce_purchase_order_item_types', 'line_item' ) );
+                do_action( 'woocommerce_after_payment_share',['order_id'=> $order->get_order_number(),'item'=>$order_items]);
+            ?>
             <div class="woocommerce-share">
                 <button id="share_face_book" class="share-link"><i class="icon-facebook"></i>share to facebook</button>
                 <button id="share_twitter" class="share-link"><i class="icon-twitter"></i>share to twitter</button>
