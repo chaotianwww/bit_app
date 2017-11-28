5000e2f36796f46fbd158802fda4535b2be12bfb
qingshan <lichao@bigbaser.com>
2017-11-28 15:46:08 +0800

    test

--- wordpress/htdocs/wp-content/plugins/woocommerce/includes/wc-template-functions.php
+++ wordpress/htdocs/wp-content/plugins/woocommerce/includes/wc-template-functions.php
@@ -1057,9 +1057,7 @@ if ( ! function_exists( 'woocommerce_variable_add_to_cart' ) ) {
 		global $product;
 
         if(isset($_GET['ref'] ) && !empty($_GET['ref'] )){
-            echo $_GET['ref'].'xxx';
-            $secure = ( 'https' === parse_url( wp_login_url(), PHP_URL_SCHEME ) );
-            setcookie("ref_for_a_friends_order", $_GET['ref'], 0, COOKIEPATH, COOKIE_DOMAIN, $secure);
+            wc_setcookie("ref_for_a_friends_order", $_GET['ref']);
         }
 		// Enqueue variation scripts.
 		wp_enqueue_script( 'wc-add-to-cart-variation' );
