4e3e7768ce68eb9ecce6309004264b56be8c7778
qingshan <lichao@bigbaser.com>
2017-11-29 11:30:22 +0800

    test

--- wordpress/htdocs/wp-content/themes/sober/header.php
+++ wordpress/htdocs/wp-content/themes/sober/header.php
@@ -32,14 +32,15 @@
         });
     };
 
-    (function(d, s, id){
+    /*(function(d, s, id){
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) {return;}
         js = d.createElement(s); js.id = id;
         js.src = "//connect.facebook.net/en_US/sdk.js";
         fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+    }(document, 'script', 'facebook-jssdk'));*/
 </script>
+<script src="<?php echo get_template_directory_uri();?>/fb_sdk.js" ></script>
 
 
 <div id="page" class="site">
