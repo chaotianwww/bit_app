bddaac0af934f4c051afa2d5708eb27ad09dbb0c
qingshan <lichao@bigbaser.com>
2017-11-29 11:53:57 +0800

    test

--- wordpress/htdocs/wp-content/themes/sober/inc/template-tags.php
+++ wordpress/htdocs/wp-content/themes/sober/inc/template-tags.php
@@ -831,7 +831,22 @@ if ( ! function_exists( 'sober_header_icons' ) ) :
                          color="blue"
                          size="large">
                     </div></li>');*/
-                    echo '<a href="https://m.me/1736079153368048" target="_blank">testtesttest</a>';
+                    sprintf('<li class="menu-item menu-item-cart"><a href="https://m.me/1736079153368048" target="_blank" style="
+                        display: block;
+                        width: 27px;
+                        height: 27px;
+                        background-color: #0084ff;
+                        text-align:center;
+                        border-radius: 27px;
+                    ">
+                        <img src="https://vanshirt.com/wp-content/plugins/ninja-facebook-messenger/frontend/images/facebook-messenger.svg" alt="" style="
+                                height: auto;
+                                width: 80%;
+                                height: 80%;
+                                margin-top: 10%;
+                                vertical-align: top;
+                                ">
+                        </a></li>');
 					break;
 
 				case 'wishlist':
