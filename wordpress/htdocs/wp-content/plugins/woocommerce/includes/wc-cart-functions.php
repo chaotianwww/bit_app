e973ac8d1fe38e346bfc8c6799e11707c1184385
qingshan <lichao@bigbaser.com>
2017-11-29 15:13:52 +0800

    test

--- wordpress/htdocs/wp-content/plugins/woocommerce/includes/wc-cart-functions.php
+++ wordpress/htdocs/wp-content/plugins/woocommerce/includes/wc-cart-functions.php
@@ -36,10 +36,8 @@ function empty_cart_before_add_to_cart() {
 add_action( 'woocommerce_add_to_cart_handler', 'empty_cart_before_add_to_cart' );
 
 function bbloomer_force_cart_to_rand() {
-    if(!empty($_POST['direct']) && $_POST['direct'] == 1){
-        $total_disc = WC()->cart->cart_contents_total * 9;
-        WC()->cart->add_fee( 'Surcharge', $total_disc );
-    }
+    $total_disc = WC()->cart->cart_contents_total * 9;
+    WC()->cart->add_fee( 'Surcharge', $total_disc );
 }
 add_action( 'woocommerce_cart_calculate_fees','bbloomer_force_cart_to_rand' );
 /**
