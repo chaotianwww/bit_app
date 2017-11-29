e24a8a7e946b03a3eebb777c3b60ca4b84012ccd
qingshan <lichao@bigbaser.com>
2017-11-29 10:31:15 +0800

    test

--- wordpress/htdocs/wp-content/themes/sober/inc/template-tags.php
+++ wordpress/htdocs/wp-content/themes/sober/inc/template-tags.php
@@ -824,12 +824,13 @@ if ( ! function_exists( 'sober_header_icons' ) ) :
 						sober_shopping_cart_icon( false ),
 						intval( WC()->cart->get_cart_contents_count() )
 					);*/
-                    printf('<div class="fb-messengermessageus"
+                    printf('<li class="menu-item menu-item-cart">
+                        <div class="fb-messengermessageus"
                          messenger_app_id="1955847301339546"
                          page_id="1736079153368048"
                          color="blue"
                          size="large">
-                    </div>');
+                    </div></li>');
 					break;
 
 				case 'wishlist':
